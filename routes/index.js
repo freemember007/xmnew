6cffd8627181028bb5a59ee949c8ca2e40d01eb1
freemem <freemember007@gmail.com>
2013-08-21 23:12:11 +0800

    seo and domain

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ exports.index = function(req, res){
 				return res.json({type: 'success', data: deals, count: count})
 			});
 		} else {
-			return res.render('main', {title: '', nextURL: '/page/' + newPageNum, data: deals })
+			return res.render('main', {title: '', keywords:'值得买, 限时抢购, 促销活动, 海淘, 优惠券, 超值, 优惠', description:'聚合最权威最丰富的值得买信息，包括淘宝值得买频道、网易惠惠网、一淘优惠购、折800值得买、爱图购、爱乐活良品等数十多个网站和博客。', nextURL: '/page/' + newPageNum, data: deals })
 		}
 	})
 }
@@ -59,7 +59,7 @@ exports.deals = function(req, res){
 		_.map(deals, function(deal){
 			deal.moment = moment(new Date( parseInt(deal._id.toString().substring( 0, 8 ), 16 ) * 1000 )).fromNow()
 		})
-		return res.render('main', {title: req.params.name + '-', nextURL: nextURL, data: deals }) //可在hash表里加模板缓存cache: true, 
+		return res.render('main', {title: req.params.name + '-', keywords: req.params.name+',值得买, 限时抢购, 促销活动, 海淘, 优惠券, 超值, 优惠', description:'聚合最权威最丰富的' + req.params.name + '值得买信息，包括淘宝值得买频道、网易惠惠网、一淘优惠购、折800值得买、爱图购、爱乐活良品等数十多个网站和博客。', nextURL: nextURL, data: deals }) //可在hash表里加模板缓存cache: true, 
 	})
 }
 
@@ -71,7 +71,7 @@ exports.detail = function(req, res){
 		if(req.params.format){
 			return res.json({type: 'success', data: deal})
 		} else {
-			return res.render('detail', {title: deal.title, data: deal })
+			return res.render('detail', {title: deal.title, keywords: (deal.brand + deal.pname + deal.model + ', 值得买').replace(/undefined/g, ''), description: deal.content.replace(/<[^>]*>/g,'').substring(0, 70), data: deal })
 		}
 	})
 }
